22fc274c83974d0383bc3f49daef52af628f46c7
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2022-08-13 17:56:10 -0500

    Create external auth
    * added functionality to login with google
    * added login page
    * added home page
    * added redux lib
    
    Resolve issue #1

---
+++ src/redux/reducers/userInfo.js
@@ -0,0 +1,13 @@
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+export const setUserInfo = createAction("user/userInfo");
+
+const initialState = {
+  userinfo: {},
+};
+
+export const userInfo = createReducer(initialState, {
+  [setUserInfo]: (state, action) => {
+    state = action.payload;
+  },
+});
